39c93e88a8bcd0a9905aac6d4813db822bc9ad2e
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-18 19:02:21 -0700

    step2

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+let navLinks =$$("nav a") //2.1 get all nav links
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );//2.2 find the link to the current page
+currentLink?.classList.add('current');//2.3 add the current class to the current page link
