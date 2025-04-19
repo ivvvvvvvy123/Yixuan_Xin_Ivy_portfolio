c49ddc2bdc46b91fd454159e99fe42c6b09ee85d
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-19 12:37:06 -0700

    step 3.1 insertadjacenthtml

--- global.js
+++ global.js
@@ -3,21 +3,22 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-let navLinks =$$("nav a") //2.1 get all nav links
-//2.2 find the link to the current page
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-//2.3 add the current class to the current page link 
-if (currentLink) {
-// or if (currentLink !== undefined)
-currentLink?.classList.add('current');
-}
+//step 2
+// let navLinks =$$("nav a") //2.1 get all nav links
+// //2.2 find the link to the current page
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+// //2.3 add the current class to the current page link 
+// if (currentLink) {
+// // or if (currentLink !== undefined)
+// currentLink?.classList.add('current');
+// }
 
+//step3.1 add navigation menu
 let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
-    // add the rest of your pages here
     { url: 'contact/', title: 'Contact' },
     { url: 'cv/', title: 'CV' },
     { url: 'https://github.com/ivvvvvvvy123', title: 'GitHub' },
@@ -27,22 +28,24 @@ const BASE_PATH = (location.hostname === "localhost" || location.hostname === "1
 ? "/"                  // Local server
 : "/Yixuan_Xin_Ivy_portfolio/";         // GitHub Pages repo name
 
-  //create a new <nav> element
+  //3.1create a new <nav> element
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+//going throug a for loop to iterate over the page and add <a> in the <nav>
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // next step: create link and add it to nav
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+//     url = !url.startsWith('http') ? BASE_PATH + url : url;
+//     let a = document.createElement('a');
+//     a.href = url;
+//     a.textContent = title;
 
-    a.classList.toggle(
-        'current',
-        a.host === location.host && a.pathname === location.pathname,
-    );
-    a.toggleAttribute('target', a.host !== location.host);
-    nav.append(a);
-}
+//     a.classList.toggle(
+//         'current',
+//         a.host === location.host && a.pathname === location.pathname,
+//     );
+//     a.toggleAttribute('target', a.host !== location.host);
+//     nav.append(a);
+// }
