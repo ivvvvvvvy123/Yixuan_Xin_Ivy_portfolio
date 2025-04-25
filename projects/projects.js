501921b5a19ded562fc4cf289cf20122ec31fe2b
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-25 00:02:21 -0700

    step 1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,9 @@
-// import { fetchJSON, renderProjects } from '../global.js';
-// const projects = await fetchJSON('../lib/projects.json');
-// const projectsContainer = document.querySelector('.projects');
-// renderProjects(projects, projectsContainer, 'h2');
+import { fetchJSON, renderProjects } from '../global.js';
 
+async function loadAndRenderProjects() {
+  const projects = await fetchJSON('../projects.json'); // adjust path if needed
+  const projectsContainer = document.querySelector('.project'); // match HTML
+  renderProjects(projects, projectsContainer, 'h2');
+}
 
-console.log('projects.js is connected!');
+loadAndRenderProjects();
