ff9129222696bdd209cb652d63bdbcf0a4c712e7
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-26 14:00:53 -0700

    step 2 update

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
-async function loadAndRenderLatestProjects() {
-  const projects = await fetchJSON('./lib/projects.json');
-  const latestProjects = projects.slice(0, 3);
-
-  const projectsContainer = document.querySelector('.projects');
-
-  projectsContainer.innerHTML = ''; 
-
-  for (const project of latestProjects) {
-    renderProjects(project, projectsContainer, 'h2');
-  }
-}
-loadAndRenderLatestProjects();
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
