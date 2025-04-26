ab551b136d379c3b866f6359a6f5b01d4257a618
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-26 13:05:02 -0700

    fix uncaught syntaxerror

--- global.js
+++ global.js
@@ -137,20 +137,3 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
 
   containerElement.appendChild(article);
 }
-// export function renderProjects(project, containerElement, headingLevel = 'h2') {
-//   // a container that we can put the generated content inside
-  
-//   containerElement.innerHTML='';//empty the old content to aovid repeated
-//   const article = document.createElement('article');
-//   article.innerHTML = `
-//     <h3>${project.title}</h3>
-//     <img src="${project.image}" alt="${project.title}">
-//     <p>${project.description}</p>
-//     `;
-//   containerElement.appendChild(article);
-//   // for(const project of projects){//begin a loop going through each object
-//   //   const heading=document.createElement('h2'); //create a new h2 title element
-//   //   heading.textContent=project.title; //put the title of each project  in the h2
-//   //   containerElement.appendChild(heading); //put the h2 element in the container so website can see the new title
-//   // }
-// }
