3552acecd65ef90ed1565529ec2008b2b7ad9a65
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-25 19:20:53 -0700

    step 1.4 update for loop

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,10 @@ import { fetchJSON, renderProjects } from '../global.js';
 async function loadAndRenderProjects() {
   const projects = await fetchJSON('../projects.json'); // adjust path if needed
   const projectsContainer = document.querySelector('.project'); // match HTML
-  renderProjects(projects, projectsContainer, 'h2');
+  projectsContainer.innerHTML='';
+  for(const project of projects){
+    renderProjects(project,projectsContainer,'h2');
+  }
 }
 
 loadAndRenderProjects();
