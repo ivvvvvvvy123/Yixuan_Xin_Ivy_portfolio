10c0f3e5bc7e8583a2e345c8458a30878338fee6
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-26 20:45:53 -0700

    fix step 3

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
-async function loadAndRenderLatestProjects() {
+async function loadAndRenderHomePage() {
   const projects = await fetchJSON('./lib/projects.json');
   const latestProjects = projects.slice(0, 3);
 
   const projectsContainer = document.querySelector('.projects');
-
   projectsContainer.innerHTML = '';
 
   for (const project of latestProjects) {
     renderProjects(project, projectsContainer, 'h2');
   }
-}
 
-loadAndRenderLatestProjects();
-const githubData = await fetchGitHubData('ivvvvvvvy123');
-const profileStats = document.querySelector('#profile-stats');
-if (profileStats) {
+  const githubData = await fetchGithubData('ivvvvvvvy123');
+  const profileStats = document.querySelector('#profile-stats');
+  if (profileStats) {
     profileStats.innerHTML = `
-          <dl>
-            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-            <dt>Followers:</dt><dd>${githubData.followers}</dd>
-            <dt>Following:</dt><dd>${githubData.following}</dd>
-          </dl>
-      `;
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
   }
+}
+loadAndRenderHomePage();
