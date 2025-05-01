a016dbfc9521d269badfaed12ba9fecffa303841
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-04-30 20:59:20 -0700

    step 1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadAndRenderProjects() {
   const projects = await fetchJSON('../lib/projects.json');
@@ -13,3 +14,9 @@ async function loadAndRenderProjects() {
 }
 
 loadAndRenderProjects();
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
