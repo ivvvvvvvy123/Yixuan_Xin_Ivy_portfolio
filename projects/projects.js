44e0a484b5096d0d12e7e458197f126d6712ae3b
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-05-01 22:11:10 -0700

    step 3.3

--- projects/projects.js
+++ projects/projects.js
@@ -10,25 +10,22 @@ async function loadAndRenderProjects() {
   }
   const projectCount = document.getElementById('project-count');
   projectCount.textContent = `Total projects: ${projects.length}`;
-
+  const rolledData=d3.rollups(
+    projects,
+    v=>v.length,
+    d=>d.year
+  );
+  const data = rolledData.map(([year, count]) => {
+    return { label: year, value: count };
+  });
 }
 
-loadAndRenderProjects();
-
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let sliceGenerator = d3.pie().value((d) => d.value);
+let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
+let arcs = arcData.map(d => arcGenerator(d));
+d3.select('#projects-plot').selectAll('*').remove();
 
 arcs.forEach((arcPath, idx) => {
   d3.select('#projects-plot')
@@ -39,21 +36,15 @@ arcs.forEach((arcPath, idx) => {
 });
 
 let legend = d3.select('.legend');
+legend.selectAll('*').remove();
 data.forEach((d, idx) => {
   legend
     .append('li')
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .attr('class','legend-item')
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
+loadAndRenderProjects();
 
-let legendContainer = document.querySelector('#legend');
-legendContainer.innerHTML = ''; // clear existing
 
-data.forEach((d, idx) => {
-  let li = document.createElement('li');
-  li.innerHTML = `
-    <span class="swatch" style="--color: ${colors(idx)}"></span>
-    ${d.label}
-  `;
-  legendContainer.appendChild(li);
-});
+
