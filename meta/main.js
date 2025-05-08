ddd9e440611979e46b8e3dbb9c276ca68359f879
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-05-08 14:11:24 -0700

    1.1

--- meta/main.js
+++ meta/main.js
@@ -1,6 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+console.log("Script is running...");
 async function loadData() {
-    const data = await d3.csv('loc.csv');
+    const data = await d3.csv('./meta/loc.csv');
     console.log(data);
     return data;
   }
