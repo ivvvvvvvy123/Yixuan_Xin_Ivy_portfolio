87d56ce57bb1e245f363767ea12828ae576baf41
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-05-08 14:25:35 -0700

    step 1.2

--- meta/main.js
+++ meta/main.js
@@ -1,9 +1,46 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-console.log("Script is running...");
 async function loadData() {
-    const data = await d3.csv('./meta/loc.csv');
-    console.log(data);
-    return data;
-  }
+const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+}));
+
+return data;
+}
+
+
+function processCommits(data) {
+    return d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
   
-  let data = await loadData();
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          // What other options do we need to set?
+          // Hint: look up configurable, writable, and enumerable
+        });
+  
+        return ret;
+      });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
