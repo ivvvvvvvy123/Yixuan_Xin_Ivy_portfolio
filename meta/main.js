635911da691c86de85b651b706dd908d93d7e71b
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-05-30 21:17:47 -0700

    step 2.1

--- meta/main.js
+++ meta/main.js
@@ -265,6 +265,7 @@ function isCommitSelected(selection, commit) {
 
 
     }
+    
 
     //lab 6 step3.3, 3.4 pisiton the tooltipnear the mouse cursor
     function updateTooltipPosition(event) {
@@ -291,8 +292,36 @@ function isCommitSelected(selection, commit) {
   .range([0, 100]);
 let commitMaxTime = timeScale.invert(commitProgress);
 let filteredCommits=commits;
+
 const commitProgressslider = document.getElementById('commit-progress');
 const selectedTime=document.getElementById("commit-time");
+
+function updateFileDisplay(filteredCommits){
+  let lines = filteredCommits.flatMap((d) => d.lines);
+  let files = d3
+  .groups(lines, (d) => d.file)
+  .map(([name, lines]) => {
+    return { name, lines };
+  });
+  let filesContainer = d3
+.select('#files')
+.selectAll('div')
+.data(files, (d) => d.name)
+.join(
+  // This code only runs when the div is initially rendered
+  (enter) =>
+    enter.append('div').call((div) => {
+      div.append('dt').append('code');
+      div.append('dd');
+    }),
+);
+
+// This code updates the div info
+filesContainer.select('dt > code').text((d) => d.name);
+filesContainer.select('dd').text((d) => `${d.lines.length} lines`);
+  updateScatterPlot(data, filteredCommits);
+  
+}
 function formatTime(date) {
   return date.toLocaleString('en-US', { dateStyle: "long",
   timeStyle: 'short' }); // Format as HH:MM AM/PM
@@ -305,8 +334,8 @@ function onTimeSliderChange(){
     const commitMaxTime =  timeScale.invert(commitProgressslider.value);
     selectedTime.textContent=formatTime(commitMaxTime);
     filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
-    updateScatterPlot(data, filteredCommits);
-    
+    updateFileDisplay(filteredCommits);
+
   }
 }
 commitProgressslider.addEventListener('input', onTimeSliderChange);
